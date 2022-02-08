442e1620aba8996ae03ab206c0976f4cd0fdb13a
bsw8220 <a49208220@gmail.com>
2022-02-08 18:20:08 +0900

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        Home
+        <div>
+            Board List
+            <div v-if="loading">Loading...</div>
+            <div v-else>
+                <div v-for="b in boards" :key="b.id">
+                    <pre>{{b}}</pre>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import {board} from '../api'
+
+export default {
+    data() {
+        return {
+            loading: false,
+            boards: [],
+        }
+    },
+    created() {
+        this.fetchData()
+    },
+    methods: {
+        fetchData() {
+            this.loading = true;
+            board.fetch()
+                .then(data => {
+                    this.boards = data
+                    console.log("auth success");
+                })
+                .finally(() => {
+                    this.loading = false
+                })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
