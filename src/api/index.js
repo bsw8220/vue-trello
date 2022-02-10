bf01ff031ac39e0e4782109b7e329b4798975881
bsw8220 <a49208220@gmail.com>
2022-02-10 18:12:44 +0900

    보드 생성 api 추가

--- src/api/index.js
+++ src/api/index.js
@@ -4,34 +4,47 @@ import router from '../router'
 const DOMAIN = 'http://localhost:3000'
 const UNAUTHORIZED = 401
 const onUnauthorized = () => {
-    router.push('/login')
+  router.push(`/login?rPath=${encodeURIComponent(location.pathname)}`)
 }
 
 const request = (method, url, data) => {
-    return axios({
-        method,
-        url: DOMAIN + url,
-        data
-    }).then(result => result.data)
+  return axios({
+    method,
+    url: DOMAIN + url,
+    data
+  })
+    .then(result => result.data)
     .catch(result => {
-        const {status} = result.response
-        if (status === UNAUTHORIZED) return onUnauthorized()
-        throw Error(result)
+      const { status } = result.response
+      if (status === UNAUTHORIZED)
+        onUnauthorized()
+      throw result.response
     })
 }
 
 export const setAuthInHeader = token => {
-    axios.defaults.headers.common['Authorization'] = token ? `Bearer ${token}` : null;
+  axios
+    .defaults
+    .headers
+    .common['Authorization'] = token
+      ? `Bearer ${token}`
+      : null;
 }
+
+const { token } = localStorage
+if (token)
+  setAuthInHeader(token)
 
 export const board = {
-    fetch() {
-        return request('get', '/boards')
-    }
+  fetch() {
+    return request('get', '/boards')
+  },
+  create(title) {
+    return request('post', '/boards', {title})
+  }
 }
-
 export const auth = {
-    login(email, password) {
-        return request('post', '/login', {email, password})
-    }
+  login(email, password) {
+    return request('post', '/login', { email, password })
+  }
 }
