32dae7762c72cc54d258992b5ac8a5bfc96c05a5
bsw8220 <a49208220@gmail.com>
2022-02-16 13:45:16 +0900

    카드 수정 로직 추가

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,39 +1,93 @@
 <template>
-  <div>
-    Card
-    <div v-if="loading">loading card ...</div>
-    <div v-else>cid: {{ cid }}</div>
-  </div>
+  <Modal>
+    <div slot="header" class="modal-card-header">
+      <div class="modal-card-header-title">
+        <input class="form-control" type="text" :value="card.title" 
+        :readonly="!toggleTitle" @click="toggleTitle=true" @blur="onBlurTitle"
+        ref="inputTitle">
+      </div>
+      <a class="modal-close-btn" href="" @click.prevent="onClose">&times;</a>
+    </div>
+    <div slot="body">
+      <h3>Description</h3>
+      <textarea  class="form-control" cols="30" rows="3" placeholder="Add a more detailed description..."
+        :readonly="!toggleDescription" @click="toggleDescription=true" @blur="onBlurDescription"
+        v-model="card.description" ref="inputDescription"></textarea>
+    </div>
+    <div slot="footer"></div>
+  </Modal>
 </template>
 
 <script>
+import Modal from './Modal.vue'
+import {mapActions, mapState} from 'vuex'
+
 export default {
+  components: {
+    Modal
+  },
   data() {
     return {
-      cid: 0,
-      loading: false,
-    };
+      toggleTitle: false,
+      toggleDescription: false
+    }
   },
-  watch: {
-    $route: {
-      handler: "fetchData",
-      immediate: true,
-    },
+  computed: {
+    ...mapState({
+      card: "card",
+      board: "board"
+    })
   },
   created() {
-    this.fetchData();
+    this.fetchCard()
   },
   methods: {
-    fetchData() {
-      this.loading = true;
-      setTimeout(() => {
-        this.cid = this.$route.params.cid;
-        this.loading = false;
-      }, 500);
+    ...mapActions([
+      'FETCH_CARD',
+      'UPDATE_CARD'
+    ]),
+    onClose() {
+      this.$router.push(`/b/${this.board.id}`)
     },
+    fetchCard(){
+      const id = this.$route.params.cid
+      this.FETCH_CARD({id})
+    },
+    onBlurTitle(){
+      this.toggleTitle = false
+      const title = this.$refs.inputTitle.value.trim()
+      if (!title) return
+      this.UPDATE_CARD({id: this.card.id, title})
+        .then(() => this.fetchCard()) 
+    },
+    onBlurDescription(){
+      this.toggleDescription = false
+      const description = this.$refs.inputDescription.value.trim()
+      if (!description) return
+      this.UPDATE_CARD({id: this.card.id, description})
+        .then(() => this.fetchCard()) 
+    }
   },
 };
 </script>
 
 <style>
+.modal-card .modal-container {
+  min-width: 300px;
+  max-width: 800px;
+  width: 60%;
+}
+.modal-card-header-title {
+  padding-right: 30px;  
+}
+.modal-close-btn {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  font-size: 24px;
+  text-decoration: none;
+}
+.modal-card-header {
+  position: relative;
+}
 </style>
