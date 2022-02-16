fd27374738f6810c0909724a4f7213810570d4d1
bsw8220 <a49208220@gmail.com>
2022-02-16 13:48:45 +0900

    카드 생성, 수정, 삭제 action 추가

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,7 +2,7 @@ import * as api from '../api'
 
 const actions = {
   ADD_BOARD(_, {title}) {
-    return api.board.create(title)
+    return api.board.create(title).then(data => data.item)
   },
   FETCH_BOARDS({ commit }) {
     return api.board.fetch().then(data => {
@@ -12,6 +12,28 @@ const actions = {
   LOGIN({ commit }, { email, password }) {
     return api.auth.login(email, password)
       .then(({accessToken}) => commit('LOGIN', accessToken))
+  },
+  FETCH_BOARD({ commit }, { id }) {
+    return api.board.fetch(id).then(data => {
+      commit('SET_BOARD', data.item)
+    })
+  },
+  ADD_CARD({ dispatch, state }, {title, listId, pos}) {
+    return api.card.create(title, listId, pos)
+      .then(() => dispatch('FETCH_BOARD', {id: state.board.id}))
+  },
+  FETCH_CARD({ commit }, {id}) {
+    return api.card.fetch(id).then(data => {
+      commit('SET_CARD', data.item)
+    })
+  },
+  UPDATE_CARD({ dispatch, state }, {id, title, description, pos, listId}) {
+    return api.card.update(id, {title, description, pos, listId})
+      .then(() => dispatch('FETCH_BOARD', {id: state.board.id}))
+  },
+  DELETE_CARD({dispatch, state}, {id}) {
+    return api.card.destroy(id)
+      .then(() => dispatch('FETCH_BOARD', {id: state.board.id}))
   }
 }
 
